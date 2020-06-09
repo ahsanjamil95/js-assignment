f450cf78728f9e23122bb002c8f1b079ca744e7e
ahsan <ahsanjamil@live.com>
2020-06-09 14:22:59 +0500

    JS
    JS Assignment

---
+++ app.js
@@ -0,0 +1,122 @@
+//==== Chapter 1 ====/
+
+// 01
+alert("Javascript Alert!");
+
+// 02
+alert("Error! Please Enter a valid Password");
+
+// 03
+alert("Welcome to JS Land... \n Happy Coding.");
+
+// 04
+alert("Welcome to JS Land");
+alert("Happy Coding!");
+
+// 05
+console.log("Hello... I can run JS through my web browser console");
+
+//06
+alert("My Name Is Ahsan");
+
+//07
+alert("Body (after your page’s HTML)");
+
+//==== Chapter 2 ====//
+
+// 01
+var username="my first variable";
+alert(username);
+
+// 02
+var myName="My Name Is Ahsan Jamil";
+alert(myName);
+
+// 03
+var jsVariable="Hello World";
+alert(jsVariable);
+
+// 04
+var userName="Ahsan Jamil";
+var age="23";
+var course="Web and Mobile Application";
+
+alert(userName);
+alert(age);
+alert(course);
+
+// 05
+var foodName="Pizza \n Pizz \n Piz \n Pi \n P";
+alert(foodName);
+
+// 06
+var email="My email address is ahsanjamil@live.com";
+alert(email);
+
+// 07
+var book="I an rying to learn from the book A smarter way to learn JavaScript";
+alert(book);
+
+// 08
+document.write("Yah! i can write HTML content through JAVASCRIPT");
+
+// 09
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+
+
+//==== Chapter 3 ====//
+
+// 01
+var myAge="Iam 23 Year old";
+alert(myAge);
+
+// 02
+var visit="You have visited this site 10 times";
+alert(visit);
+
+// 03
+var birthYear=1997;
+document.write("My Birth year is" + birthYear + "<br> Data type of my declair variable is number");
+
+// 04
+document.write("Ahsan Jamil ordered 5 T-shirt(s) on Ebay Clothing store.");
+
+
+//==== Chapter 4 ====//
+
+// 01
+var title= "<b>Rules for naming JS variables</b>";
+var ques1= "Variable names can only contain number, $ and_ . For Example: $my_1stVariable";
+var ques2= "Variables must begin with a $name, _name or name. For example $name, _name or name";
+var ques3= "Variable names are case sencetive";
+var ques4= "Variable names should not be JS Key word";
+
+document.write(title + "<br>" + ques1 + "<br>" + ques2 + "<br>" + ques3 + "<br>" + ques4);
+
+
+//==== Chapter 5 ====//
+
+// 01
+var a=44;
+var b=20;
+var sumof=a+b;
+document.write("sum of 44 and 20 is equal to: "+ sumof);
+
+// 02
+var a=44;
+var b=20;
+var sum=a+b;
+var subtract=a-b;
+var multiply= a*b;
+var divide= a/b;
+var moduls=44%20;
+document.write("sum of 44 and 20 is : "+ sum +"<br>");
+document.write("subtract of 44 and 20 is : "+ subtract +"<br>");
+document.write("multiply of 44 and 20 is : "+ multiply +"<br>");
+document.write("divide of 44 and 20 is : "+ divide +"<br>");
+document.write("moduls of 44 and 20 is : "+ moduls +"<br>");
+
+// 03
+ 
+
+
